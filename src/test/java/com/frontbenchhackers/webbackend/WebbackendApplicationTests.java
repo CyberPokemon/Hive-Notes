edc2976840e5d77abf5ee45dd437501bc6660099
CyberPokemon <imoncoding@gmail.com>
2025-02-14 12:12:38 +0530

    Initial backend project setup

---
+++ src/test/java/com/frontbenchhackers/webbackend/WebbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontbenchhackers.webbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
