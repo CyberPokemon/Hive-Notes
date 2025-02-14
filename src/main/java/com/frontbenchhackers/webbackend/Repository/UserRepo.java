12ff213e68dcfede8e0fa3db27e71c8f25849ce4
CyberPokemon <imoncoding@gmail.com>
2025-02-14 13:00:20 +0530

    added content to userrepo

--- src/main/java/com/frontbenchhackers/webbackend/Repository/UserRepo.java
+++ src/main/java/com/frontbenchhackers/webbackend/Repository/UserRepo.java
@@ -1,4 +1,10 @@
 package com.frontbenchhackers.webbackend.Repository;
 
-public class UserRepo {
+import com.frontbenchhackers.webbackend.Model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users,Integer> {
+    Users findByUsername(String username);
 }
